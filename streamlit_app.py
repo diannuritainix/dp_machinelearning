739291acf0162df60e1674629b3878c7937b5f43
diannuritainix <diannurita.inix@gmail.com>
2025-09-19 15:38:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Machine Learning App 🕹')
 
 st.info('This is a Machine Learning App!')
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+df
