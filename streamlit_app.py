77fc8bbbba88b51336a56ce7bf76a16e5d297b72
diannuritainix <diannurita.inix@gmail.com>
2025-10-02 09:42:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,4 +50,7 @@ with st.expander('Input features'):
   st.write('**Combine Penguins Data**')
   input_penguins
 
-
+#Encode
+encode = ['island','gender']
+df_penguins = pd.get_dummies(input_penguins, prefix=encode)
+df_penguins
